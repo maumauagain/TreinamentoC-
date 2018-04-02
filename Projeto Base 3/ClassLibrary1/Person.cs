b9d221a991f3796d34b1c0afb56c5cf091d20646
maumauagain <amauri.martins.jr@hotmail.com>
2018-04-02 13:37:28 -0300

    Atividade 3

---
+++ Projeto Base 3/ClassLibrary1/Person.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Activity.ObjectModel
+{
+    public class Person : ProjetoBase.Base
+    {
+        public DateTime BirthDate { get; set; }
+        //public Person FatherName = new Person();
+        public string Email { get; set; }
+
+        public static Person getPerson()
+        {
+            return new Person
+            {
+            };
+        }
+    }
+}
---
+++ Projeto Base 3/ClassLibrary1/Project.cs
@@ -0,0 +1,34 @@
+﻿using ProjetoBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Activity.ObjectModel
+{
+    public class Project : Base
+    {
+        public String Code { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EstimatedEndDate { get; set; }
+
+        public DateTime RealEndDate {get; set; }
+
+        public Person Responsible = new Person();
+
+        public Tasks[] taskList = new Tasks[10];
+
+        public Person[] userList = new Person[10];
+
+        public static Project GetProject()
+        {
+            return new Project
+            {
+            };
+        }
+
+        public bool MarkFinished() {
+            return false;
+        }
+    }
+}
